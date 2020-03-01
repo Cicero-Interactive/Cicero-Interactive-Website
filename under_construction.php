d5cd8ab4901404d22dcb9253dafe07beb0c6eff8
Julian Burner <burnerjulian@gmail.com>
2020-03-02 00:04:31 +0100

    Added basic "under construction" page

---
+++ under_construction.php
@@ -0,0 +1,12 @@
+---
+title: Under Construction
+---
+
+<div style="height: 100%; width: 100%;">
+	<div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); text-align: center;">
+		<h1>This website is under construction!</h1>
+		<p>
+			We are currently rebuilding our website from the ground up to provide a better user experience. It should be ready by March 10th. Thank you for your patience!
+		</p>
+	</div>
+</div>
