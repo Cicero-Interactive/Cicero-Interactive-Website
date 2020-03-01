c4c770b091f73af4925fa2abe08b1860b80db1e0
Julian Burner <burnerjulian@gmail.com>
2020-03-02 00:11:06 +0100

    Rewrote nav with jekyll

--- _includes/nav.php
+++ _includes/nav.php
@@ -1,32 +1,27 @@
-<?php 
-	if (isset($_GET['logout'])) {
-		session_destroy();
-        session_unset();
-        exit('<script>window.location.replace("./");</script>');
-    }
-?>
+{% assign page.navitems =  site.data.navitems | process_navitems %}
 
 <nav>
     <div style="max-width: 2000px; margin: auto">
-        <ul class="ShowOnBigScreen">
-            <li><a href="./"><img src="assets/images/CILogo.png"></a></li>
-            <li><a {% if page.title == "About" %}class="active"{% endif %} href="about.php">About</a></li>
-            <li><a {% if page.title == "Tools" %}class="active"{% endif %} href="tools.php">Tools</a></li>
-            <li class="dropdown">
-                <a class="dropbtn {% if page.title == "French Invaders" or page.title == "Latin Wars: Call of Rupert" %}class="active"{% endif %} ?>">Games ▾</a>
-                <div class="dropdown-content">
-                    <a href="lwcor.php">Latin Wars: Call of Rupert</a>
-                    <a href="fi.php">French Invaders</a>
-                </div>
-            </li>
-            <li><a {% if page.title == "News" %}class="active"{% endif %} href="news.php">News</a></li>
-            <li class="dropdown">
-                <a class="dropbtn">Other Services ▾</a>
-                <div class="dropdown-content">
-                    <a href="https://lateinparadies.de.cool/" target="_blank">Lateinparadies</a>
-                    <a href="">Pettri.de (Work in Progress)</a>
-                </div>
-            </li>
+        <!-- Navbar for wider devices -->
+        <ul class="showOnBigScreen">
+            <li><a href="/"><div><img src="assets/images/CILogo.png" style="filter: drop-shadow(1px 1px 1px black) drop-shadow(-1px -1px 1px black);"></div></a></li>
+            {% for navitem in site.data.navitems %}
+                <li {% if navitem.dropdown %} class="dropdown" {% endif %}>
+                    <a  {% if page.title == navitem.en %}
+                            class="active"
+                        {% endif %}
+                        {% unless navitem.dropdown %} href="{{ navitem.dest }}" {% endunless %}>
+                        {{ navitem.en }} {% if navitem.dropdown %} ▾ {% endif %}
+                    </a>
+                    {% if navitem.dropdown %}
+                        <div class="dropdown-content">
+                            {% for dropdownitem in navitem.dropdown %}
+                                <a href="{{ dropdownitem.dest }}">{{ dropdownitem.en }}</a>
+                            {% endfor %}
+                        </div>
+                    {% endif %}
+                </li>
+            {% endfor %}
             <?php if (!isset($_SESSION["username"])) { ?>
                 <li style="float:right"><a {% if page.title == "Register" %}class="active"{% endif %} href="register.php">Register</a></li>
                 <li style="float:right"><a {% if page.title == "Log In" %}class="active"{% endif %} href="login.php?redirect=.{{page.url}}">Log In</a></li>
@@ -49,28 +44,44 @@
                 </li>
             <?php ;} ?>
         </ul>
-        <ul class="ShowOnSmallScreen">
-            <li><a href="./"><img src="assets/images/CILogo.png" style="height: 64px"></a></li>
-            <li style="float:right"><a onclick="openNav()" style="font-size: 40px; padding: 7px 22px">&#9776;</a></li>
+        <ul class="showOnSmallScreen">
+            <li><a href="./"><img src="assets/images/CILogo.png" style="height: 50px"></a></li>
+            <li style="float:right"><a onclick="openNav()" style="font-size: 30px; padding: 7px 22px">&#9776;</a></li>
         </ul>
     </div>
-</nav>
 
-<div class="sidepnl ShowOnSmallScreen" id="sidepnl">
-    <a class="closebtn" onclick="closeNav()">&times;</a>
-    <a {% if page.title == "About" %}class="active"{% endif %} href="about.php">ABOUT</a>
-    <a {% if page.title == "Tools" %}class="active"{% endif %} href="about.php">TOOLS</a>
-    <span class="sidepnlCategory">GAMES</span>
-    <div class="sidepnlSub">
-        <a {% if page.title == "Latin Wars: Call of Rupert" %}class="active"{% endif %} href="lwcor.php">> Latin Wars: Call of Rupert</a>
-        <a {% if page.title == "French Invaders" %}class="active"{% endif %} href="fi.php">> French Invaders</a>
+    <!-- Mobile panel for small screen sizes -->
+    <div class="sidepnl showOnSmallScreen" id="sidepnl">
+        <a class="closebtn" onclick="closeNav()">&times;</a>
+        {% for navitem in site.data.navitems %}
+            {% unless navitem.dropdown %}
+                <a  {% if page.title == "{{ navitem.en }}" %}
+                        class="active"
+                    {% endif %}
+                    {% unless navitem.dropdown %} href="{{ navitem.dest }}" {% endunless %}>
+                    {{ navitem.en | upcase }}
+                </a>
+            {% else %}
+                <span
+                    {% if page.title == "{{ navitem.en }}" %}
+                        class="active"
+                    {% endif %}
+                    class="sidepnlCategory">
+                    {{ navitem.en | upcase }}
+                </span>
+                <div class="sidepnlSub">
+                    {% for dropdownitem in navitem.dropdown %}
+                        <a  {% if page.title == "{{ navitem.en }}" %}
+                                class="active"
+                            {% endif %}
+                            href="{{ dropdownitem.dest }}">
+                            > {{ dropdownitem.en }}
+                        </a>
+                    {% endfor %}
+                </div>
+            {% endunless %}
+        {% endfor %}
+        <a {% if page.title == "Log In" %}class="active"{% endif %} href="login.php?redirect=<?php echo $page ?>">LOG IN</a>
+        <a {% if page.title == "Register" %}class="active"{% endif %} href="register.php">REGISTER</a>
     </div>
-    <a {% if page.title == "News" %}class="active"{% endif %} href="news.php">NEWS</a>
-    <span class="sidepnlCategory">OTHER SERVICES</span>
-    <div class="sidepnlSub">
-        <a href="https://lateinparadies.de.cool/" target="_blank">> Lateinparadies</a>
-        <a href="">> Cicero's Powerpoint Presentations</a>
-    </div>
-    <a {% if page.title == "Log In" %}class="active"{% endif %} href="login.php?redirect=<?php echo $page ?>">LOG IN</a>
-    <a {% if page.title == "Register" %}class="active"{% endif %} href="register.php">REGISTER</a>
-</div>
+</nav>
