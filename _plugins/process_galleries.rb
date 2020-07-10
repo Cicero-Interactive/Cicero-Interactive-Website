9ab56c147837a5f74abfc8b8f9c4fa7c875a038c
Cicero Interactive <burnerjulian@gmail.com>
2020-07-10 13:22:59 +0200

    Added product information to slideshow images

--- _plugins/process_galleries.rb
+++ _plugins/process_galleries.rb
@@ -4,40 +4,55 @@ module Process_galleries
 		site = Jekyll.sites.first
 
 		for gallery in galleries do
-			puts(gallery["dir"])
+			puts("Processig gallery: " + gallery["dir"])
+			# Loop thorough all files in specified gallery dir
 			Dir.foreach(gallery["dir"]) do |filename|
-				next if filename == '.' or filename == '..'
-					included = false
-					if gallery["images"]
-						for image in gallery["images"] do
-							included = false
-							if image["name"] == filename
-								included = true
-								if image["path"] == nil
-									image["path"] = site.baseurl + "/" + gallery["dir"] + filename
-								end
-								break
+				# Skip parent folder entry
+				if filename == '.' or filename == '..'
+					next
+				end
+				included = false
+				if gallery["images"]
+					for image in gallery["images"] do
+						included = false
+						if image["name"] == filename
+							# Image from folder does exist in gallery's images array
+							included = true
+							if image["path"] == nil
+								image["path"] = site.baseurl + "/" + gallery["dir"] + filename
+							end
+							if image["theme"] == nil
+								image["theme"] = "light"
 							end
 						end
+						
 					end
-					if included == false && gallery["visible"] == "all"
-						gallery["images"].append("path" => site.baseurl + "/" + gallery["dir"] + filename)
-					end
+				end
+				# If image from folder does not exist in array, append it. This check is excluded from the loop above for it not to continue indefinetely
+				if included == false && gallery["visible"] == "all"
+					gallery["images"].append("path" => site.baseurl + "/" + gallery["dir"] + filename)
 				end
 			end
 
+			i = 0
 			for image in gallery["images"] do
+				i += 1
+				# Set animation direction, if not specified
 				if image["direction"] == nil
 					image["direction"] == "rtl"
 				end
-				if image["direction"] == nil
-					image["direction"] == "rtl"
-				end
-				if image["direction"] == nil
-					image["direction"] == "rtl"
+
+				if image["product"] == nil
+					for product in gallery["products"] do
+						if product["images"].include?(i)
+							# Set product
+							image["product"] = product["short"]
+						end
+					end
 				end
 			end
 		end
 	end
+end
 
 Liquid::Template.register_filter(Process_galleries)
